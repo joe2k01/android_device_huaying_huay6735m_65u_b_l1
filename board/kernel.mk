12205cc0a7e36cd5b3850cb7c1ed6f51c537d4fc
Joe <joe2k01dev@gmail.com>
2017-03-25 17:35:02 +0100

    Stock values

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,7 +2,7 @@
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00080000 --ramdisk_offset 0x04000000 --tags_offset 0xE000000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
 
 # Kernel properties
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
